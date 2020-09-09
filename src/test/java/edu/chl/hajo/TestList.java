1a3e0e667f4bdf2e4e31401177ca44477cab11bc
erikwessman <erikwessman10@gmail.com>
2020-09-09 11:13:52 +0200

    Initial commit

---
+++ src/test/java/edu/chl/hajo/TestList.java
@@ -0,0 +1,64 @@
+package edu.chl.hajo;
+
+import static java.lang.System.out;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/*
+    
+     This is a test class (for some other class, in this case the List class)
+
+ */
+public class TestList {
+
+    @Test
+    public void testAdd() {
+        List l = new List();
+        l.add(1);                        // Call method to test
+        assertTrue(l.getLength() == 1);  // The logical check
+    }
+
+    @Test
+    public void testRemove() {
+        List l = new List();
+        l.add(5);
+        l.add(10);
+        int len = l.getLength();
+        int n = l.remove();
+        assertTrue(n == 10);
+        assertTrue(l.getLength() == (len-1));
+    }
+
+    @Test
+    public void testGet() {
+        List l = new List();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        assertTrue(l.get(4) == 5);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testGetBadIndex() {
+        List list = new List();
+        list.get(-1); // Exception!!!
+    }
+
+    @Test
+    public void testCopy() {
+        List l = new List();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        List newL = l.copy();
+        assertTrue(newL.equals(l));
+    }
+
+}
